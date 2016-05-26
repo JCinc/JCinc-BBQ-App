0cf91b1ee8b00b3f4e7f106f5d4a4db9b2d920c3
justinechiu <chiujustine@gmail.com>
2016-05-26 11:38:49 -0400

    stuff

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -41,8 +41,8 @@ BBQApp.getRecipeData = function () {
 
 // Drink Finder variables, storing the Key and URL (which I've concatenated together)
 BBQApp.drinksKey = 'MDo0NjQ5MjEzNC0yMWY4LTExZTYtYTIxNy01ZjMzOTgzMzVmODU6djFobWhkNTlrWFhnTVBPemI4VWZHUUlFZE5IQUtTSlJUYmE3';
-BBQApp.drinksApiUrl = 'http://lcboapi.com/products?access_key=';
-// ISSUE CONNECTING KEY
+BBQApp.drinksApiUrl = 'http://lcboapi.com/products';
+
 // Drink Finder AJAX call
 BBQApp.getDrinkData = function () {
 	$.ajax({
