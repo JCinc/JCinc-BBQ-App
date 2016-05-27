7f7f1e1044e62ade5b466c6ba41831c7102071ab
justinechiu <chiujustine@gmail.com>
2016-05-27 16:36:21 -0400

    updated styles,index,script

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -112,7 +112,7 @@ BBQApp.nearestLCBO = function(location) {
 
 BBQApp.postalSearch = function() {
 	$('.postalIntro').append('<h2>Find your nearest store</h2>');
-	$('.postalSearch').append('<form class="postalCodeForm"><input type="text" placeholder="Postal Code" id="txtPostalCode" maxlength="6"><button type="submit" class="btn triggerSearch" id="btnSearchStores">Search</button></form>');
+	$('.postalSearch').append('<form class="postalCodeForm"><input type="text" placeholder="e.g. A1A1A1" id="txtPostalCode" maxlength="6"><button type="submit" class="btn" id="btnSearchStores">Search</button></form>');
 	// On submit of the postal code, we store the result in a variable
 	$('.postalCodeForm').on('submit', function(e){
 		e.preventDefault();
@@ -236,7 +236,7 @@ BBQApp.foodOntoPage = function(i, recipeName, recipeImage, recipeLink, recipeCoo
 	// Then a link
 	$('#food-item' + i).append("<a target='_blank' href=" + recipeLink + ">" + "<p>View recipe</p>" + "</a>");
 	// Then the cook time
-	var foodDiv = $('#food-item' + i).append("<h5>Ready in " + recipeCookTime + " minutes</h5>");
+	var foodDiv = $('#food-item' + i).append("<p>Ready in " + recipeCookTime + " minutes</p>");
 
 	// BBQApp.drinksOntoPage(foodDiv);
 };
@@ -269,7 +269,7 @@ BBQApp.displayDrinkResults = function (results) {
  // BBQApp.drinksOntoPage() will allow for us to implement our recipe items onto the page
 BBQApp.drinksOntoPage = function(i, drinkName) {
 	// And then a name
-	$('#food-item' + i).append("<h1>" + drinkName + "</h1>");
+	$('#food-item' + i).append("<h3>" + "Pairs with this drink: " + drinkName + "</h3>");
 };
 
 
