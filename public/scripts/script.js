2fbfc3cb9f35cd4c7ca95db934830134778d3f88
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-05-27 10:40:32 -0400

    changes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -190,7 +190,7 @@ BBQApp.displayDrinkResults = function (results) {
 };
 
 // BBQApp.drinksOntoPage() will allow for us to implement our recipe items onto the page
-BBQApp.drinksOntoPage = function (i) {
+BBQApp.drinksOntoPage = function (i, drinkName) {
 	// And then a name
 	$('#food-item' + i).append("<h1>" + drinkName[i] + "</h1>");
 };
